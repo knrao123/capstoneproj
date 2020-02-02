89c58c10eb8d24d70394b53b2f219ade3c312fa8
knrao123 <knrao.hal@gmail.com>
2020-02-02 20:16:31 +0530

    my first capstone test

---
+++ HelloCapstoneProjectCourse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Create a new repository on  Github account  Coursera_Capstone
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
